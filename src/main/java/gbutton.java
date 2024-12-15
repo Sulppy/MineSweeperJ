892c07e7d237d66ed782dd228a995e3e5e8c89d3
Sulppy <nikolajpit@mail.ru>
2024-12-15 15:22:15 +0300

    Added a class constructor for setting standard values

--- src/main/java/gbutton.java
+++ src/main/java/gbutton.java
@@ -1,8 +1,21 @@
-import io.qt.widgets.QPushButton;
+import io.qt.core.Qt;
+import io.qt.gui.QPalette;
 
 public class gbutton {
     int num;
     boolean isMine;
     boolean isFlagged = false;
-    QPushButton qbtn;
+    qGameButtons qbtn;
+
+    gbutton() {
+        isFlagged = false;
+        isMine = false;
+        num = -1;
+        qbtn = new qGameButtons();
+        qbtn.setFixedSize(30, 30);
+        qbtn.setText("");
+        QPalette p = new QPalette();
+        p.setBrush(QPalette.ColorRole.Button, Qt.GlobalColor.white);
+        qbtn.setPalette(p);
+    }
 }
